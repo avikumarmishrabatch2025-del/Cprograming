fc88cb5c3e7298c0097c307ca8b35c000fddd24e
avikumarmishrabatch2025-del <avikumar.mishra.batch2025@sitnagpur.siu.edu.in>
2025-11-13 19:21:00 +0530

    practical6.c

--- practical6.c
+++ practical6.c
@@ -2,45 +2,40 @@
 #include <math.h>
 
 int main() {
-    int choice;
-    printf("1. Simple Interest\n2. Prime Number\n3. Armstrong Number\nEnter choice: ");
-    scanf("%d", &choice);
+    int ch;
+    printf("1. Simple Interest\n2. Prime\n3. Armstrong\nEnter choice: ");
+    scanf("%d", &ch);
 
-    switch (choice) {
-        case 1: {
-            float p, r, t, si;
-            printf("Enter P, R, T: ");
-            scanf("%f %f %f", &p, &r, &t);
-            si = (p * r * t) / 100;
-            printf("Simple Interest = %.2f\n", si);
-            break;
-        }
-        case 2: {
-            int n, i, flag = 0;
-            printf("Enter number: ");
-            scanf("%d", &n);
-            for (i = 2; i <= n / 2; i++)
-                if (n % i == 0) { flag = 1; break; }
-            if (flag == 0 && n > 1) printf("Prime\n");
-            else printf("Not Prime\n");
-            break;
-        }
-        case 3: {
-            int n, sum = 0, temp, r;
-            printf("Enter number: ");
-            scanf("%d", &n);
-            temp = n;
-            while (n > 0) {
-                r = n % 10;
-                sum += r*r*r;
-                n /= 10;
-            }
-            if (sum == temp) printf("Armstrong\n");
-            else printf("Not Armstrong\n");
-            break;
-        }
-        default:
-            printf("Invalid choice\n");
+    if (ch == 1) {
+        float p, r, t;
+        printf("Enter P, R, T: ");
+        scanf("%f %f %f", &p, &r, &t);
+        printf("Simple Interest = %.2f\n", (p * r * t) / 100);
     }
+
+    else if (ch == 2) {
+        int n, i, f = 0;
+        printf("Enter number: ");
+        scanf("%d", &n);
+        for (i = 2; i < n; i++)
+            if (n % i == 0) f = 1;
+        if (f == 0 && n > 1) printf("Prime\n");
+        else printf("Not Prime\n");
+    }
+
+    else if (ch == 3) {
+        int n, temp, r, sum = 0;
+        printf("Enter number: ");
+        scanf("%d", &n);
+        temp = n;
+        while (n > 0) {
+            r = n % 10;
+            sum += r*r*r;
+            n /= 10;
+        }
+        if (sum == temp) printf("Armstrong\n");
+        else printf("Not Armstrong\n");
+    }
+
     return 0;
 }
