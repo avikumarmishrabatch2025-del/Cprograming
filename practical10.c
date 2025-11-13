7e26c51b29eaf78b8733ff2fe5596588b45ea800
avikumarmishrabatch2025-del <avikumar.mishra.batch2025@sitnagpur.siu.edu.in>
2025-11-13 19:14:24 +0530

    practical10.c

--- practical10.c
+++ practical10.c
@@ -2,34 +2,22 @@
 #include <string.h>
 #include <math.h>
 
-void sortStrings(char arr[][50], int n) {
-    char temp[50];
-    for (int i = 0; i < n - 1; i++)
-        for (int j = i + 1; j < n; j++)
-            if (strlen(arr[i]) > strlen(arr[j])) {
-                strcpy(temp, arr[i]);
-                strcpy(arr[i], arr[j]);
-                strcpy(arr[j], temp);
-            }
-}
-
 int main() {
-    int n;
-    printf("Enter number of strings: ");
-    scanf("%d", &n);
-    char arr[n][50];
-    for (int i = 0; i < n; i++) {
-        printf("Enter string %d: ", i + 1);
-        scanf("%s", arr[i]);
-    }
-    sortStrings(arr, n);
-    printf("\nStrings sorted by length:\n");
-    for (int i = 0; i < n; i++) printf("%s\n", arr[i]);
+    char s1[20], s2[20];
+    printf("Enter two strings: ");
+    scanf("%s %s", s1, s2);
+    if (strlen(s1) > strlen(s2))
+        printf("%s is longer\n", s1);
+    else
+        printf("%s is longer\n", s2);
 
-    float r, *p = &r;
+    float r, *p;
     printf("\nEnter radius: ");
     scanf("%f", &r);
-    printf("Area = %.2f\n", M_PI * (*p) * (*p));
-    printf("Circumference = %.2f\n", 2 * M_PI * (*p));
+    p = &r;
+    printf("Area = %.2f\n", 3.14 * (*p) * (*p));
+    printf("Circumference = %.2f\n", 2 * 3.14 * (*p));
+
     return 0;
 }
+
