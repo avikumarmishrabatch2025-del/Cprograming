cf00d9c3bc24fdf59f31b4250d36aaa25421cd1f
avikumarmishrabatch2025-del <avikumar.mishra.batch2025@sitnagpur.siu.edu.in>
2025-11-13 19:24:01 +0530

    practical9.c

--- practical9.c
+++ practical9.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 union Data {
     int i;
@@ -8,12 +9,16 @@ union Data {
 
 int main() {
     union Data data;
+
     data.i = 10;
     printf("Integer: %d\n", data.i);
+
     data.f = 3.14;
     printf("Float: %.2f\n", data.f);
+
     strcpy(data.str, "Hello");
     printf("String: %s\n", data.str);
-    // Note: All share same memory
+
     return 0;
 }
+
